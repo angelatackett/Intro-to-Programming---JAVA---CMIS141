276bc086a4ab1e95a30476b4ab88a48c5d7f3920
Angela Tackett <117610120+angelatackett@users.noreply.github.com>
2023-01-17 02:40:45 -0500

    discussion response added

---
+++ Week 1 - Variables & Outputs/TackettResponseDiscussionWk1.java
@@ -0,0 +1,20 @@
+public class TackettResponseDiscussionWk1 {
+	public static void main(String[] args) {
+		int countries; //create a number for countries visited
+		String name; //variable for my name
+		countries = 0; //declare number of countries
+		name = "Russell Krizan"; //name myself as the name
+	System.out.println("Hello! My name is " + name);
+	System.out.print("I have visited " + countries + " countries other than the United States.");
+//           ***ANGELA'S RESPONSE*** 
+		String nombre = "Angela";                                    // Declare variables
+		String nickname = "Ang";     
+		int countryNum = 6;                                          // # of countries
+			System.out.println("\n\nHello, " + name + "!\nI'm "      // Begin response statement
+			+ nombre + ", but you can call me \"" 
+			+ nickname + "\".\nI have been to a total of " 
+			+ countryNum + " countries.\nThese include: "); 
+		String[] C = {"Italy","Spain","Dubai","Cyprus","Oman","Pakistan"};   // Define array
+		int arraylength = C.length;                                          // assign an integer reflecting # of variables in array
+			for(int i = 0; i<arraylength; i++) {System.out.println(C[i]);    // For Loop to print each variable in array
+			}	}	}                                                        // close For Loop | close method | close class 
