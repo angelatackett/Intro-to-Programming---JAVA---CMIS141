ef3a8436aeb946db52706c3fd1cd4f65fa47119e
Angela Tackett <117610120+angelatackett@users.noreply.github.com>
2023-01-16 23:36:10 -0500

    Create DiscussionOne.java

---
+++ Week 1 - Variables & Outputs/DiscussionOne.java
@@ -0,0 +1,19 @@
+/** A Java program to satisfy
+ * Week 1 - Variables & Outputs
+ * discussion requirements.
+ * Developer : A_Tackett 
+ * CMIS141 6385 16JAN23
+ */
+
+ public class DiscussionOne {
+
+	public static void main(String[] args) {
+
+        String mrsAwesomeSauce = "Angela Tackett";
+        double gasMyDay = 1.15;
+    
+        System.out.println("Greetings Earthlings, " + mrsAwesomeSauce
+            + " here!\n\n I come in peace!! I remember back in my day when rocket fuel was only $" 
+        	+ gasMyDay + " a gallon!\n\n Oh, how the lightyears have changed...");
+    } // end of method
+} // end of class
