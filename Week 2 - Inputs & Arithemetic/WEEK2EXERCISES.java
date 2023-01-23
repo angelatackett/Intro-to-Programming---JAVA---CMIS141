86523419f71bd461af6612b91686458434c981a4
Angela Tackett <117610120+angelatackett@users.noreply.github.com>
2023-01-23 01:20:53 -0500

    update file

--- Week 2 - Inputs & Arithemetic/WEEK2EXERCISES.java
+++ Week 2 - Inputs & Arithemetic/WEEK2EXERCISES.java
@@ -1,6 +1,9 @@
 import java.util.*;  //import util for Scanner
 
 public class WEEK2EXERCISES {
+        /**
+         * @param args
+         */
         public static void main(String[] args){
 
             /* Exercise 1 - Program which prompts user for two numbers,
@@ -14,7 +17,7 @@ public class WEEK2EXERCISES {
             int result;
 
             //prompt user for input
-            System.out.print("Enter a number: ");  
+            System.out.print("Exercise 1\n\nEnter a number: ");  
             //assign input to variable              
             no_1 = in.nextInt();                                   
 
@@ -27,6 +30,20 @@ public class WEEK2EXERCISES {
             //print results -- formatted to include comma utilizing format specifier : %,d
             System.out.printf("Result of sum: %,d", result);
 
-    }
+
+
+            /* Exercise 2 - Program which prompts user for two integers,increments
+            them by one and prints each on a separate line to the console */
+
+            int n1, n2, n3, n4;
+            System.out.println("\n\nExercise 2\n\nEnter Two Numbers" 
+               + "(Press enter after each):");
+            n1 = in.nextInt();
+            n2 = in.nextInt();
+            n3 = ++n1;
+            n4 = ++n2;
+            System.out.print("\n" + n3 + "\n" + n4);
+
+        }
     
 }
