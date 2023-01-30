3060b3cf99bbeaebd064fff8bd7ef72165c9f7dd
Angela Tackett <117610120+angelatackett@users.noreply.github.com>
2023-01-30 16:23:46 -0500

    Update TackettAssignment2.java

--- Week 3 - Booleans & Conditions/TackettAssignment2.java
+++ Week 3 - Booleans & Conditions/TackettAssignment2.java
@@ -3,9 +3,9 @@ Developer Angela Tackett
 CMIS 141 30JAN2023 Assignment 2
 ------------------------------------
 This program prompts the user for two
-different numbers and a mathematical operation
-symbol to perform requested function utilizing 
-a switch.
+different numbers within a specified range 
+and a mathematical operation symbol to perform
+requested function utilizing a switch.
 
 It provides valid inputs along with results or
 otherwise displays an error message.
@@ -31,16 +31,26 @@ public class TackettAssignment2 {
         no_1 = no.nextInt();
         no_2 = no.nextInt();
 
-        // while loop ensures inputs are within specified range
-        while (no_1 < 200 || no_1 > 1000 || no_2 < 200 || no_2 > 1000) {
-            System.out.println("Invalid Range");
-            System.out.print("Enter two numbers from 200 to 1000 \n"
-            +   "[separate numbers with a 'space']: ");
-            no_1 = no.nextInt();
-            no_2 = no.nextInt();
-
-        }
-
+        // while loop ensures inputs are within allowed range
+        while (true) {
+            if (no_1 < 200 || no_1 > 1000) {
+                System.out.println("Invalid Range");
+                System.out.print("Enter two numbers from 200 to 1000 \n"
+                +   "[separate numbers with a 'space']: ");
+                no_1 = no.nextInt();
+                no_2 = no.nextInt();
+            }
+            else if (no_2 < 200 || no_2 > 1000) {
+                System.out.println("Invalid Range");
+                System.out.print("Enter two numbers from 200 to 1000 \n"
+                +   "[separate numbers with a 'space']: ");
+                no_1 = no.nextInt();
+                no_2 = no.nextInt();             
+            }
+            else {
+                break;
+            }
+        }   
         // prompt for mathematical operation input
         System.out.print("Enter one of the following mathematical operation symbols"
          + "\n[ + - * /] : ");
@@ -50,24 +60,20 @@ public class TackettAssignment2 {
         // switch
         switch (mathFunc) {
 
-        case '+' : compute = no_1 + no_1;
+        case '+' : compute = no_1 + no_2;
             System.out.print(no_1 + " + " + no_2 + " = " + compute);
             break;
-        case '-' : compute = no_1 - no_1;
+        case '-' : compute = no_1 - no_2;
             System.out.print(no_1 + " - " + no_2 + " = " + compute);
             break;
-        case '*' : compute = no_1 * no_1;
+        case '*' : compute = no_1 * no_2;
             System.out.print(no_1 + " * " + no_2 + " = " + compute);
             break;
-        case '/' : compute = (double)no_1 % no_1;
+                    // [below] type cast int to double to bypass int division [drops remainder]                
+        case '/' : compute = (double) no_1 / (double) no_2; 
             System.out.print(no_1 + " / " + no_2 + " = " + compute);
             break;
         default  : System.out.print("Error - invalid operation input."); 
-        }
-
-        no.close();     
-
-
-    
+        } no.close();         
     }
 }
