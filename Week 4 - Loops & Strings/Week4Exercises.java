446445a46d69802ed505c7f53bc03179998922f3
Angela Tackett <117610120+angelatackett@users.noreply.github.com>
2023-02-04 23:00:39 -0500

    Update Week4Exercises.java

--- Week 4 - Loops & Strings/Week4Exercises.java
+++ Week 4 - Loops & Strings/Week4Exercises.java
@@ -6,6 +6,18 @@ public class Week4Exercises {
 
 //EXERCISE 1 
 
+/* Prompt user for an integer and then print the below pattern
+ * using nested while loops.
+ * 
+ * Enter integer value: 5
+ *
+ * 1
+ * 1 2
+ * 1 2 3
+ * 1 2 3 4
+ * 1 2 3 4 5
+ */
+
 // initialize scanner
         // Scanner in = new Scanner(System.in);
 
@@ -30,8 +42,43 @@ public class Week4Exercises {
         // in.close();
 
 
-//
+//Exercise 2
 
+/* Prompt user for two even integers and print all even
+values between those two using while loop.
+
+example:
+Enter two even integer values separated by space: 10 20
+12
+14
+16
+18*/ 
+        // initialize scanner
+        // Scanner scan = new Scanner(System.in);
+        // System.out.print("Enter two even integer values separated by space:");
+        // int num1 = scan.nextInt();
+        // int num2 = scan.nextInt();
+
+        // int counter1 = num1 + 2;
+        // while (counter1 < num2) {
+        //     System.out.println(counter1);
+        //     counter1+=2;
+        // }
+        // scan.close();
+
+// Exercise 3
+/* Prompt user for integer value and use while loop to determine reverse
+ * value.
+ * 
+ * Output example:
+ * Enter an integer value: 76543
+ * Reversed value = 34567
+*/
+
+        Scanner input = new Scanner(System.in);
+
+
+            input.close();
         }
 
     }
