84c9c9670878cb7222502a95cec2ab1b21ec9f12
Angela Tackett <117610120+angelatackett@users.noreply.github.com>
2023-02-04 19:06:38 -0500

    created Week4

---
+++ Week 4 - Loops & Strings/Week4Exercises.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Week4Exercises {
+    
+    public static void main(String args[]) {
+
+//EXERCISE 1 
+
+// initialize scanner
+        // Scanner in = new Scanner(System.in);
+
+        // // prompt user
+        // System.out.print("Enter integer value: ");
+        
+        // // identify input variable
+        // int num = in.nextInt();         
+        // int counter1 = 1;
+        // int counter2;
+
+        // // Loopsies
+        // while (counter1 <= num){
+        //     counter2 = 1;
+        //     while (counter2 <= counter1) {
+        //         System.out.print(counter2 + " ");
+        //         counter2++;
+        //     }
+        //     System.out.println();
+        //     counter1++;
+        // }
+        // in.close();
+
+
+//
+
+        }
+
+    }
+
