3fded911d4b2f300ba6b9250395c6d42c6ace2e5
Angela Tackett <117610120+angelatackett@users.noreply.github.com>
2023-02-16 22:25:31 -0500

    Create Week5Exercises.java

---
+++ Week 5 - FOR Loops/Week/Week5Exercises.java
@@ -0,0 +1,91 @@
+
+import java.util.Scanner;
+
+public class Week5Exercises {
+    
+    public static void main(String [] args) {
+
+        /*Exercise 1 - Prompt user for a positive integer(num), keep prompting
+        if not greater than 0, and then use for-loop to print "Hello" to console
+        num times.
+        */
+
+        // Scanner in = new Scanner(System.in);
+        // int num;
+        // int x;
+        // String h = "Hello";
+
+        // // do-while loop 
+        // do {
+        //     System.out.print("Enter positive number: ");
+        //     num = in.nextInt();
+        //         if (num <= 0) {
+        //             System.out.println("Number must be greater than 0. Try again.");
+        //         }
+        // } while (num <= 0);
+        
+        // for (x = 1 ; x <= num ; x++ ) {
+        //        System.out.println(h);
+        //        in.close();        
+        // }  //end of Exercise #1
+
+
+        /*Exercise 2 - Prompt user for the number of students. For each student
+         * prompts for 3 quiz grades, calculate and display the quiz values average.
+         * Use for-loop in your code.
+         */
+
+        // Scanner in = new Scanner(System.in);
+
+        // int x;
+        // int stu_no;
+        // int quiz1, quiz2, quiz3;
+        // double quizAvg;
+
+        // do {
+        // System.out.print("Enter the number of students: ");
+        // stu_no = in.nextInt();
+        // System.out.println();
+        // } while (stu_no <= 0);
+
+        //     for (x=1; x <= stu_no; x++) {    
+        //         System.out.println("Student " + x + " data:"); 
+        //         System.out.print("     Enter 3 quiz values separated by space (0-100): ");
+            
+        //         // storing quiz values
+        //         quiz1 = in.nextInt();
+        //         quiz2 = in.nextInt();
+        //         quiz3 = in.nextInt();
+        //         // calculate average of quiz inputs
+        //         quizAvg = (quiz1+quiz2+quiz3)/3.0;
+        //         // display calculation results
+        //         System.out.printf("     Quiz average: %.1f", quizAvg);
+        //         System.out.println();
+        //         System.out.println();
+
+        // }
+        // in.close();         
+    
+        /*Excercise 3- Prompt user for positive integer value (length of the side) and then
+        use nested for-loops to output the diamond pattern.
+        */    
+    
+        Scanner in = new Scanner(System.in);
+
+        int side;
+        
+        for (int side=1; side > 0; side =) {
+        System.out.print("Enter desired diamond side length: ");
+        side = in.nextInt(); 
+        
+        }
+
+
+
+    }
+        
+
+         
+
+        }
+
